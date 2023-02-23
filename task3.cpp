57f7e54e72db913977c24b5fa76be282a4d6f6d1
nikkk1912 <moskalevskiy1@gmail.com>
2023-02-23 21:33:22 +0200

    task3 complete

--- task3.cpp
+++ task3.cpp
@@ -5,27 +5,34 @@ using namespace std;
 
 int main() {
   int size;
+  auto k = 0;
   cin >> size;
 
   vector<int> first(size);
+  vector<int> second;
 
   for (auto i = 0; i < size; i++) {
     cin >> first[i];
   }
 
+  for (int i=0; i<first.size(); i++) 
+        second.push_back(first[i]); 
+
 
   for (auto i = 0; i < size; i++) {
+    
     auto j = first.at(i);
     if (j < 0) {
-      first.erase(first.end() - i);
-      first.push_back(j);
+      second.erase(second.begin() + i - k);
+      second.push_back(j);
+      k++;
     }
   }
 
-  if (!first.empty()) {
-    cout << first[0];
-    for (int i = 1; i < first.size(); i++) {
-      cout << " " << first[i];
+  if (!second.empty()) {
+    cout << second[0];
+    for (int i = 1; i < second.size(); i++) {
+      cout << " " << second[i];
     }
   }
   return 0;
