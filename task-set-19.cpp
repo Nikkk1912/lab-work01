7d099f9d436203efe06d2661c436eec056b9b069
nikkk1912-laptop <moskalevskiy1@gmail.com>
2023-03-01 09:18:29 +0300

    task-set-19 complete

--- task-set-19.cpp
+++ task-set-19.cpp
@@ -4,5 +4,25 @@
 using namespace std;
 
 int main(){
+
+    int size, min, max;
+    int n = 0;
+    set<int> s1;
+
+    cin >> size >> min >> max;
     
+    for (int i = 0; i < size; i++) {
+        int val;
+        cin >> val;
+        s1.insert(val);
+    }
+
+    for (int val : s1) {
+        if (min <= val && val < max) {
+            n++;
+        }
+    }
+
+    cout << n;
+
 }
